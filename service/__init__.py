f6141521c7075cd7a2e8dcf34ad6690e70f3110d
reksa <reksa@users.noreply.github.com>
2025-10-20 12:00:11 +0700

    Add files via upload

---
+++ service/__init__.py
@@ -0,0 +1,42 @@
+"""
+Package: service
+
+Package for the application models and service routes
+This module creates and configures the Flask app and sets up the logging
+and SQL database
+"""
+import sys
+from flask import Flask
+from service import config
+from service.common import log_handlers
+
+# NOTE: Do not change the order of this code
+# The Flask app must be created
+# BEFORE you import modules that depend on it !!!
+
+# Create the Flask aoo
+app = Flask(__name__)  # pylint: disable=invalid-name
+
+# Load Configurations
+app.config.from_object(config)
+
+# Dependencies require we import the routes AFTER the Flask app is created
+# pylint: disable=wrong-import-position, wrong-import-order, cyclic-import
+from service import routes, models        # noqa: F401, E402
+from service.common import error_handlers, cli_commands  # noqa: F401, E402
+
+# Set up logging for production
+log_handlers.init_logging(app, "gunicorn.error")
+
+app.logger.info(70 * "*")
+app.logger.info("  P E T   S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(70 * "*")
+
+try:
+    models.init_db(app)  # make our sqlalchemy tables
+except Exception as error:  # pylint: disable=broad-except
+    app.logger.critical("%s: Cannot continue", error)
+    # gunicorn requires exit code 4 to stop spawning workers when they die
+    sys.exit(4)
+
+app.logger.info("Service initialized!")
